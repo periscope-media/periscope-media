b4dbe1ca87adc5fd91fa1fd6c99f61c1db71d4f9
braungoodson <braungoodson@gmail.com>
2018-09-18 14:55:14 -0400

    added todo sql

--- db/migrations/create-tables.sql
+++ db/migrations/create-tables.sql
@@ -1,17 +1,5 @@
-create table if not exists users (
-  uid_ serial not null primary key,
-  uname varchar(16) not null unique,
-  upassword varchar(16) not null,
-  ubirthday timestamp not null
-);
-
-create table if not exists news (
-  nid serial not null primary key,
-  ntitle varchar(256) not null,
-  ndescription varchar(512) not null,
-  nauthor varchar(128) not null,
-  nurl varchar(2083) not null,
-  nimage varchar(2083) not null,
-  npublished timestamp not null,
-  nfound timestamp not null
-);
+create table if not exists todos {
+  tid serial not null primary key,
+  ttitle varchar(256) not null,
+  tcomplete boolean not null
+}
